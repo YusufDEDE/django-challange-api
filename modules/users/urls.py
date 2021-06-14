3827297ed1b2f689e7e80af400550ed33a5b902c
Yusuf Dede <yusufdede95@hotmail.com>
2021-06-15 00:09:40 +0300

    user 'users/me' endpoint add with BasicAuthentication

--- modules/users/urls.py
+++ modules/users/urls.py
@@ -1,7 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import CreateUserView
+from .views import CreateUserView, MeUserView
+from django.urls import path, include
 
 router = DefaultRouter()
-router.register(r'signup', CreateUserView)
+router.register(r'signup', CreateUserView, basename='signup/')
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('me/', MeUserView.as_view(), name='ex')
+]
--- modules/users/views.py
+++ modules/users/views.py
@@ -1,9 +1,33 @@
+from .serializers import UserSerializer
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin
-from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 
 class CreateUserView(CreateModelMixin, GenericViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+
+class MeUserView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {
+            'me': list(User.objects.filter(username=request.user).values(
+                'username',
+                'first_name',
+                'last_name',
+            )),  # `django.contrib.auth.User` instance.
+        }
+        return Response(content)
+
+
+class SearchUserView(APIView):
+    pass

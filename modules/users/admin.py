5be4b317fce137db1054fc29622ce4c1c4e175ef
Yusuf Dede <yusufdede95@hotmail.com>
2021-06-16 00:51:07 +0300

    admin site csv export and list_display, search_filter etc.. add

--- modules/users/admin.py
+++ modules/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
+from csvexport.actions import csvexport
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    exclude = ('user', )
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'phone', 'about_us')
+    search_fields = ('username', 'first_name', 'last_name', 'about_us')
+    actions = [csvexport]
